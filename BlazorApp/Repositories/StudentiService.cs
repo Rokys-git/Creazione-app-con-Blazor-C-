35ad442bd6e1db701a4ba6384a28f6ec33e68711
Rocco Pugliese <rocco98pugliese@gmail.com>
2026-02-23 18:33:02 +0100

    Add files via upload

---
+++ BlazorApp/Repositories/StudentiService.cs
@@ -0,0 +1,50 @@
+﻿using BlazorApp.Components.Data;
+using BlazorApp.Models;
+using BlazorApp.wwwroot.Services.interfaces;
+using BlazorApp.Components.Data;
+using BlazorApp.Models;
+using BlazorApp.wwwroot.Services.interfaces;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp.Services.Repositories;
+using BlazorApp.Components.Pages.ListaStudenti;
+
+
+namespace BlazorApp.Services.Repositories
+{
+    public class StudentiService : IStudentiService
+    {
+        private readonly WebAppContext _context;
+
+
+        public StudentiService(WebAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddStudenteAsync(Studente studente)
+        {
+            await _context.Studenti.AddAsync(studente);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteStudenteAsync(int id)
+        {
+            Studente? studente = await _context.Studenti.FindAsync(id);
+            if (studente != null)
+            {
+                _context.Studenti.Remove(studente);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Studente>> GetAllStudentiAsync() => await _context.Studenti.ToListAsync();
+
+        public async Task<Studente?> GetStudenteByIdAsync(int id) => await _context.Studenti.FirstOrDefaultAsync(s => s.id == id);
+
+        // Esercizio per domani: implementare UpdateStudenteAsync
+        public Task UpdateStudenteAsync(Studente studente)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorApp/wwwroot/Services/Interfaces/IStudentiService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp.wwwroot.Services.interfaces
+{
+    public interface IStudentiService
+    {
+        Task<List<Studente>> GetAllStudentiAsync();
+        Task<Studente?> GetStudenteByIdAsync(int id);
+        Task AddStudenteAsync(Studente studente);
+        Task UpdateStudenteAsync(Studente studente);
+        Task DeleteStudenteAsync(int id);
+    }
+}
